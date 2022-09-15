abd6cf4b44accd81368dd7411bcaaab434fbde90
wangdian <36665764+wangdianyh@users.noreply.github.com>
2022-09-14 22:07:08 -0300

    add form validation

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,59 +1,108 @@
 <template>
-    <form class="container">
-        <p>{{ email.address }}</p>
-        <div class="col-sm-10">
-            <label>Send Email to: </label>
-            <input type="text" v-model="email.address" placeholder="youremail">
+    <form class="container"
+          @submit.prevent='sendEmail'>
+        <div class="col-sm-10 mb-3">
+            <label class="form-label">Send Email to: </label>
+            <input class='form-control'
+                type="text" 
+                v-model="state.email.address" 
+                placeholder="youremail">
+            <span class="form-text"
+                v-if="v$.email.address.$error">
+                {{ v$.email.address.$errors[0].$message }}
+            </span>
         </div>
-        <div class="col-sm-10">
-            <label>Set timer: </label>
-            <input type="text" v-model="timer" placeholder="ativate time, by default is 30s">
+        <div class="col-sm-10 mb-3">
+            <label class="form-label">Set timer: </label>
+            <input class="form-control"
+                type="text" 
+                v-model="timer" 
+                placeholder="ativate time, by default is 30s">
+            <span class="form-text">
+                ativate timer, by default is 30s.
+            </span>
         </div>
-        <div class="col-sm-10">
-            <label>Title: </label>
-            <input type="text" placeholder="email title" required>
+        <div class="col-sm-10 mb-3">
+            <label class="form-label">Title: </label>
+            <input class='form-control'
+                type="text" 
+                placeholder="email title" 
+                v-model="state.email.title">
+            <span class="form-text" 
+                v-if="v$.email.title.$error">
+                {{ v$.email.title.$errors[0].$message }}
+            </span>
         </div>
-        <div class="col-sm-10">
-            <label>Content: </label>
-            <textarea type="text" placeholder="email content"></textarea>
+        <div class="col-sm-10 mb-3">
+            <label class="form-label">Content: </label>
+            <textarea class="form-control" 
+                type="text" 
+                placeholder="email content"
+                v-model="state.email.content"></textarea>
+            <span class="form-text"
+                v-if="v$.email.content.$error">
+                {{ v$.email.content.$errors[0].$message }}
+            </span>
         </div>
-        <button :disabled="timerActive" @click="sendEmail">Email me!</button>
-    </form>
-    
+        <button class="btn btn-primary"
+            :disabled="timerActive">Email me!</button>
+    </form> 
     
 </template>
 
 <script>
+import useVuelidate from '@vuelidate/core'
+import { required, email, minLength } from '@vuelidate/validators'
+import { reactive, computed} from 'vue'
 export default {
     name: 'Button',
-    data() {
-        let email = {
-            address: "",
-            title: "",
-            content: ""
-        }
+    setup() {
+        const state = reactive({
+            email: {
+                address: "",
+                title: "",
+                content: ""
+            }
+        })
+
+        const rules = computed(() => {
+            return {
+                email: {
+                    address: { required, email },
+                    title: { required },
+                    content: { required, minLenght: minLength(7) }
+                }
+            }
+        })
+
+        const v$ = useVuelidate(rules, state)
+
         return {
-            timer: 3, // ms
+            state,
+            v$,
+        }
+    },
+    data() {
+        return {
+            timer: 30, // ms
             timerActive: false,
-            email
         }
     },
     methods: {
-        sendEmail(e) {
-            console.log(e)
+        // submit email
+        sendEmail() {
             // check fields
-            if (!this.timerActive) {
+            this.v$.$validate()
+            if (this.v$.$error) {
+                console.log(this.v$.$errors)
+            } else if (!this.timerActive) {
                 this.timerActive = !this.timerActive
-                console.log(this.email, this.timer, this.timerActive)
+                console.log(this.state, this.timer, this.timerActive)
                 setTimeout(() => {
                     console.log(`${this.timer} later...`)
                     this.timerActive = false
                 }, this.timer * 1000)      
-            } else {
-                console.log("timer is active, click is disabled")
-                
-            }
-            
+            }  
         }
     }
 }
