acf500b4f4e1e42dedfd094f9a88124cc9e3053e
wangdian <36665764+wangdianyh@users.noreply.github.com>
2022-09-15 23:04:22 -0300

    add title

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,51 +1,55 @@
 <template>
-    <form class="container"
+    <form class="container mt-3"
           @submit.prevent='sendEmail'>
-        <div class="col-sm-10 mb-3">
-            <label class="form-label">Send Email to: </label>
-            <input class='form-control'
-                type="text" 
-                v-model="state.email.address" 
-                placeholder="youremail">
-            <span class="form-text"
-                v-if="v$.email.address.$error">
-                {{ v$.email.address.$errors[0].$message }}
-            </span>
-        </div>
-        <div class="col-sm-10 mb-3">
-            <label class="form-label">Set timer: </label>
-            <input class="form-control"
-                type="text" 
-                v-model="timer" 
-                placeholder="ativate time, by default is 30s">
-            <span class="form-text">
-                ativate timer, by default is 30s.
-            </span>
-        </div>
-        <div class="col-sm-10 mb-3">
-            <label class="form-label">Title: </label>
-            <input class='form-control'
-                type="text" 
-                placeholder="email title" 
-                v-model="state.email.title">
-            <span class="form-text" 
-                v-if="v$.email.title.$error">
-                {{ v$.email.title.$errors[0].$message }}
-            </span>
-        </div>
-        <div class="col-sm-10 mb-3">
-            <label class="form-label">Content: </label>
-            <textarea class="form-control" 
-                type="text" 
-                placeholder="email content"
-                v-model="state.email.content"></textarea>
-            <span class="form-text"
-                v-if="v$.email.content.$error">
-                {{ v$.email.content.$errors[0].$message }}
-            </span>
-        </div>
-        <button class="btn btn-primary"
-            :disabled="timerActive">Email me!</button>
+        <fieldset>
+            <legend>Button Click with 30s inactive every time...</legend>
+
+            <div class="col-sm-10 mb-3">
+                <label class="form-label">Send Email to: </label>
+                <input class='form-control'
+                    type="text" 
+                    v-model="state.email.address" 
+                    placeholder="youremail">
+                <span class="form-text"
+                    v-if="v$.email.address.$error">
+                    {{ v$.email.address.$errors[0].$message }}
+                </span>
+            </div>
+            <div class="col-sm-10 mb-3">
+                <label class="form-label">Set timer: </label>
+                <input class="form-control"
+                    type="text" 
+                    v-model="timer" 
+                    placeholder="ativate time, by default is 30s">
+                <span class="form-text">
+                    ativate timer, by default is 30s.
+                </span>
+            </div>
+            <div class="col-sm-10 mb-3">
+                <label class="form-label">Title: </label>
+                <input class='form-control'
+                    type="text" 
+                    placeholder="email title" 
+                    v-model="state.email.title">
+                <span class="form-text" 
+                    v-if="v$.email.title.$error">
+                    {{ v$.email.title.$errors[0].$message }}
+                </span>
+            </div>
+            <div class="col-sm-10 mb-3">
+                <label class="form-label">Content: </label>
+                <textarea class="form-control" 
+                    type="text" 
+                    placeholder="email content"
+                    v-model="state.email.content"></textarea>
+                <span class="form-text"
+                    v-if="v$.email.content.$error">
+                    {{ v$.email.content.$errors[0].$message }}
+                </span>
+            </div>
+            <button class="btn btn-primary"
+                :disabled="timerActive">Email me!</button>
+        </fieldset>
     </form> 
     
 </template>
